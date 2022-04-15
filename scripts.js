d6186c6f0cfe0afdf2c962676289576b0afc79d1
darklinkzito <andre_luiz_7777@hotmail.com>
2022-04-15 11:37:46 -0300

    Jogo da velha completo

---
+++ scripts.js
@@ -0,0 +1,118 @@
+let jogador, vencedor = null;
+let vencedorSelecionado = document.getElementById('vencedor-selecionado');
+let jogadorSelecionado = document.getElementById('jogador-selecionado');
+let quadrados = document.getElementsByClassName('quadrado');
+
+
+mudarJogador(value='X');
+
+
+function escolherQuadrado(id) {    
+    if (vencedor !== null) {
+        return;
+    }
+    let quadrado = document.getElementById(id);
+    quadrado.innerHTML = jogador;
+    quadrado.style.color = 'black';
+
+    if (jogador === "X") {
+        jogador = 'O';
+    }else{
+        jogador = 'X';
+    }
+    mudarJogador(jogador);
+    checaVencedor();
+}
+
+function mudarJogador(value) {
+    jogador = value;
+    jogadorSelecionado.innerHTML = jogador;
+}
+
+function checaVencedor() {
+    let quadrado1 = document.getElementById(1);
+    let quadrado2 = document.getElementById(2);
+    let quadrado3 = document.getElementById(3);
+    let quadrado4 = document.getElementById(4);
+    let quadrado5 = document.getElementById(5);
+    let quadrado6 = document.getElementById(6);
+    let quadrado7 = document.getElementById(7);
+    let quadrado8 = document.getElementById(8);
+    let quadrado9 = document.getElementById(9);
+
+    if (checaSequencia(quadrado1, quadrado2, quadrado3)) {
+        mudarCorQuadrado(quadrado1, quadrado2, quadrado3);
+        mudaVencedor(quadrado1);
+        return;
+    }
+    if (checaSequencia(quadrado4, quadrado5, quadrado6)) {
+        mudarCorQuadrado(quadrado4, quadrado5, quadrado6);
+        mudaVencedor(quadrado4);
+        return;
+    }
+    if (checaSequencia(quadrado7, quadrado8, quadrado9)) {
+        mudarCorQuadrado(quadrado7, quadrado8, quadrado9);
+        mudaVencedor(quadrado7);
+        return;
+    }
+    if (checaSequencia(quadrado1, quadrado4, quadrado7)) {
+        mudarCorQuadrado(quadrado1, quadrado4, quadrado7);
+        mudaVencedor(quadrado1);
+        return;
+    }
+    if (checaSequencia(quadrado2, quadrado5, quadrado8)) {
+        mudarCorQuadrado(quadrado2, quadrado5, quadrado8);
+        mudaVencedor(quadrado2);
+        return;
+    }
+    if (checaSequencia(quadrado3, quadrado6, quadrado9)) {
+        mudarCorQuadrado(quadrado3, quadrado6, quadrado9);
+        mudaVencedor(quadrado3);
+        return;
+    }
+    if (checaSequencia(quadrado1, quadrado5, quadrado9)) {
+        mudarCorQuadrado(quadrado1, quadrado5, quadrado9);
+        mudaVencedor(quadrado1);
+        return;
+    }
+    if (checaSequencia(quadrado3, quadrado5, quadrado7)) {
+        mudarCorQuadrado(quadrado3, quadrado5, quadrado7);
+        mudaVencedor(quadrado3);
+        return;
+    }
+}
+
+function mudaVencedor(quadrado) {
+    vencedor = quadrado.innerHTML;
+    vencedorSelecionado.innerHTML = vencedor;
+    jogador
+}
+function mudarCorQuadrado(quadrado1, quadrado2, quadrado3) {
+    quadrado1.style.background = 'green';
+    quadrado2.style.background = 'green';
+    quadrado3.style.background = 'green';
+}
+
+
+function checaSequencia(quadrado1, quadrado2, quadrado3) {
+    let eIgual = false;
+
+    if (quadrado1.innerHTML !== '-' && quadrado1.innerHTML === quadrado2.innerHTML && quadrado2.innerHTML === quadrado3.innerHTML) {
+        eIgual = true;
+    }
+
+    return eIgual;
+}
+
+function reiniciar() {
+    vencedor = null;
+    vencedorSelecionado.innerHTML = '';
+
+    for(let i = 1; i <=9; i++){
+        let quadrado = document.getElementById(i);
+        quadrado.style.background = '#eee';
+        quadrado.style.color = '#eee';
+        quadrado.innerHTML = '-';
+    }
+    mudarJogador(value = 'X');
+}
